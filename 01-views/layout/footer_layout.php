63a741b7c6c5f212c834cf1dd80dc131aba81667
Charlie <webcharlie@gmail.com>
2025-08-21 23:48:24 -0300

    Ocultar por ID - OK | Calculos revisar | falta agregar fila de valor fijos

--- 01-views/layout/footer_layout.php
+++ 01-views/layout/footer_layout.php
@@ -1,6 +1,6 @@
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-    <b>Versión: </b><!--FECHA-AUTO--><span>250819-2151</span><!--/FECHA-AUTO-->
+    <b>Versión: </b><!--FECHA-AUTO--><span>250821-2346</span><!--/FECHA-AUTO-->
     </div>
     <strong>Copyright &copy; 2022 <a href="#">ECOTECHOS S.R.L</a>.</strong> Todos los derechos reservados
     <span><?php echo '<br>BASE_URL= '.BASE_URL;?></span>
