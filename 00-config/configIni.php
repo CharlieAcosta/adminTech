0935e15c934f3bde0e38ecfeb829c92d7bb1bd72
Charlie A <charlieacosta@outlook.com>
2026-02-19 17:02:43 -0300

    DOCKER | Inicilialización en pc restaurada

--- 00-config/configIni.php
+++ 00-config/configIni.php
@@ -1,30 +1,26 @@
 <?php
 //  filename: configIni.php path: 00-config/configIni.php
+
 // BASE DE DATOS
-if( isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] == 'development' )
-{
-	define('DB_SERVER', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'techos'); 
-	define('COLUMN_TYPE', 'COLUMN_TYPE');
+if (getenv('APP_ENV') === 'development') {
 
-} 
-else
-{ 
-	define('DB_SERVER', 'localhost');
-	define('DB_USER', 'c1401312_techos');
-	define('DB_PASS', 'Eco853Techos');
-	define('DB_NAME', 'c1401312_techos');
-	define('COLUMN_TYPE', 'column_type');
+    define('DB_SERVER', 'db');       // ← importante en Docker
+    define('DB_USER', 'root');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'techos'); 
+    define('COLUMN_TYPE', 'COLUMN_TYPE');
+
+} else {
+
+    define('DB_SERVER', 'localhost');
+    define('DB_USER', 'c1401312_techos');
+    define('DB_PASS', 'Eco853Techos');
+    define('DB_NAME', 'c1401312_techos');
+    define('COLUMN_TYPE', 'column_type');
 }
-
-
-
 
 // SESION
 define('URL_LOGIN', '../01-views/login.php');
-define('SESION_TIME', 7200); // Establece duración máxima de la sesión en segundos (3600 = 1 hora)
-
-define('BASE_PATH', __DIR__ . '/../'); // Esto sube un nivel desde `00-config` hacia la raíz
+define('SESION_TIME', 7200);
+define('BASE_PATH', __DIR__ . '/../');
 ?>
