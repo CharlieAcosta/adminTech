4d4405d14fa97ddab8771f6116a106ea5c33d8e6
Charlie A <charlieacosta@outlook.com>
2026-05-10 12:37:50 -0300

    VERSION | Actualiza footer a main_v0.9.0-rc.1

--- 01-views/layout/footer_layout.php
+++ 01-views/layout/footer_layout.php
@@ -1,6 +1,6 @@
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-    <b>Versión: </b><!--FECHA-AUTO--><span>260118-2003</span><!--/FECHA-AUTO-->
+    <b>Versión: </b><!--FECHA-AUTO--><span>main_v0.9.0-rc.1</span><!--/FECHA-AUTO-->
     </div>
     <strong>Copyright &copy; 2022 <a href="#">ECOTECHOS S.R.L</a>.</strong> Todos los derechos reservados
     <?php
